26afb73d37211b8c619853db9f6ed8704386d62c
Subham Deb <subham.deb.1253@gmail.com>
2023-04-25 16:34:38 +0530

    adding actual dir and tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import BannerIndex from 'components/bannerIndex';
+import BannerIndex from '../../Components/bannerIndex';
 
 const inter = Inter({ subsets: ['latin'] })
 
